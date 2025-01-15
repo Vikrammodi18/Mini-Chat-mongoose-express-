2a56fd34613e7fcaead2e8ce00dd865434a73aa1
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-01-15 19:52:57 +0530

    edit and delete facility

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require("path")
 const Chat = require('./models/chat.js')
+var methodOverride = require('method-override')
+
 const app = express()
 
 main().then((res)=>console.log("connected to Mongoose database"))
@@ -11,10 +13,12 @@ async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp')
 }
 
+//middleWares
 app.use(express.static('public'))
 app.set("views",path.join(__dirname,"views"))
 app.set("view engine","ejs")
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
 
 
 const port = 3000
@@ -25,7 +29,6 @@ app.get('/',(req,res)=>{
 app.get('/chats',async(req,res)=>{
     let chats = await Chat.find({})
     res.render('index',{chats})
-    // console.log(chats)
 })
 //create route
 app.get('/chats/new',(req,res)=>{
@@ -43,6 +46,27 @@ app.post('/chats',async(req,res)=>{
     })
     newChat.save().then((res)=>console.log(res))
     .catch((err)=>console.log(err))
+    res.redirect('/chats')
+})
+//edit page
+app.get("/chats/:id/edit",async (req,res)=>{
+    const{id} = req.params;
+    const userDetails = await Chat.findById(id)
+    res.render("edit",{userDetails})
+    
+})
+//update message
+app.patch("/chats/:id",async (req,res)=>{
+    const{from,to,msg} = req.body;
+    const {id} = req.params
+    const updatedData = await Chat.findByIdAndUpdate(id,{msg:msg})
+    res.redirect("/chats")
+    
+})
+//delete chat
+app.get('/chats/:id/delete', async (req,res)=>{
+    const{id}= req.params;
+    deletedChat = await Chat.findByIdAndDelete(id)
     res.redirect('/chats')
 })
 app.listen(port,(req,res)=>{
